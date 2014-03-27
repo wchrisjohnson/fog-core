d5f5b425e88f97bba2d9d0e1cb62e828796fc74e
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-27 00:45:58 -0400

    make it look like Evan's version

--- lib/fog/identity.rb
+++ lib/fog/identity.rb
@@ -5,16 +5,32 @@ module Fog
       self.new(:provider => provider)
     end
 
+    # def self.new(attributes)
+    #   # attributes = attributes.dup # Prevent delete from having side effects
+    #   # case provider = attributes.delete(:provider).to_s.downcase.to_sym
+    #   # if self.providers.include?(provider)
+    #   #   require "fog/#{provider}/identity"
+    #   #   return Fog::Identity.const_get(Fog.providers[provider]).new(attributes)
+    #   # end
+    #   # raise ArgumentError.new("#{provider} has no identity service")
+    #   require "fog/openstackcommon/identity_v2"
+    #   return Fog::Identity::V2::OpenStackCommon.new(attributes)
+    # end
+
     def self.new(attributes)
-      # attributes = attributes.dup # Prevent delete from having side effects
-      # case provider = attributes.delete(:provider).to_s.downcase.to_sym
-      # if self.providers.include?(provider)
-      #   require "fog/#{provider}/identity"
-      #   return Fog::Identity.const_get(Fog.providers[provider]).new(attributes)
-      # end
-      # raise ArgumentError.new("#{provider} has no identity service")
-      require "fog/openstackcommon/identity_v2"
-      return Fog::Identity::V2::OpenStackCommon.new(attributes)
+      attributes = attributes.dup # Prevent delete from having side effects
+      provider = attributes.delete(:provider).to_s.downcase.to_sym
+
+      unless providers.include?(provider)
+        raise ArgumentError.new("#{provider} has no identity service")
+      end
+
+      require "fog/#{provider}/identity"
+      begin
+        Fog::Identity.const_get(Fog.providers[provider]).new(attributes)
+      rescue
+        Fog::const_get(Fog.providers[provider]).const_get("Identity").new(attributes)
+      end
     end
 
     def self.providers
