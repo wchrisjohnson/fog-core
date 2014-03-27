5ffa411629f8df34c7c4e808292d0f90f684c77c
Chris Johnson <wchrisjohnson@gmail.com>
2014-03-27 16:07:00 -0400

    let's see what's happening

--- lib/fog/identity.rb
+++ lib/fog/identity.rb
@@ -18,8 +18,14 @@ module Fog
     # end
 
     def self.new(attributes)
+
+      puts "ATTRIBUTES: #{attributes.to_yaml}"
+      puts "self.providers: #{self.providers.to_yaml}"
+      puts "Fog.providers: #{Fog.providers.to_yaml}"
+
       attributes = attributes.dup # Prevent delete from having side effects
       provider = attributes.delete(:provider).to_s.downcase.to_sym
+      puts "PROVIDER: #{provider}"
 
       unless self.providers.include?(provider)
         raise ArgumentError.new("#{provider} has no identity service")
@@ -27,8 +33,10 @@ module Fog
 
       require "fog/#{provider}/identity"
       begin
+        puts "Inside BEGIN"
         Fog::Identity.const_get(Fog.providers[provider]).new(attributes)
       rescue
+        puts "Inside RESCUE"
         Fog::const_get(Fog.providers[provider]).const_get("Identity").new(attributes)
       end
     end
